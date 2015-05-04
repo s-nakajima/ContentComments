8ab35fdc8e2dc2e57532d8f9c0fc483d804e897e
Mitsuru Mutaguchi <akagane99@gmail.com>
2015-05-04 19:22:11 +0900

    Modified component

--- Controller/Component/ContentCommentsComponent.php
+++ Controller/Component/ContentCommentsComponent.php
@@ -73,10 +73,51 @@ class ContentCommentsComponent extends Component {
 	}
 
 /**
+ * コメントする
+ *
+ * @param string $pluginKey プラグインキー
+ * @param string $contentKey コンテンツキー
+ * @return bool 成功 or 失敗
+ */
+	public function comment($pluginKey, $contentKey) {
+		// コンテンツコメントの処理名をパースして取得
+		if (!$process = $this->parseProcess()) {
+			return false;
+		}
+
+		// 登録・編集・承認
+		if ($process == $this::PROCESS_ADD ||
+			$process == $this::PROCESS_EDIT ||
+			$process == $this::PROCESS_APPROVED) {
+
+			// dataの準備
+			$data = $this->readyData($process, $pluginKey, $contentKey);
+
+			// コンテンツコメントのデータ保存
+			if (!$this->controller->ContentComment->saveContentComment($data)) {
+				if (!$this->controller->handleValidationError($this->controller->ContentComment->validationErrors)) {
+					// コンテンツコメント編集表示を開く
+					//$results['isDisplayEdit'] = $frameId . '_' . $this->data['contentComment']['id'];
+					$this->log($this->controller->validationErrors, 'debug');
+				}
+			}
+
+			// 削除
+		} elseif ($process == $this::PROCESS_DELETE) {
+			// コンテンツコメントの削除
+			if (!$this->controller->ContentComment->deleteContentComment($this->controller->data['contentComment']['id'])) {
+				return false;
+			}
+		}
+		//unset($this->data['ContentComment']['comment']);
+		return true;
+	}
+
+/**
  * コメントの処理名をパースして取得
  *
  * @throws BadRequestException
- * @return mixed status on success, false on error
+ * @return int どの処理
  */
 	public function parseProcess() {
 		if ($matches = preg_grep('/^process_\d/', array_keys($this->controller->data))) {
@@ -94,5 +135,48 @@ class ContentCommentsComponent extends Component {
 		}
 
 		return $process;
+	}
+
+/**
+ * dataの準備
+ *
+ * @param int $process どの処理
+ * @param string $pluginKey プラグインキー
+ * @param string $contentKey コンテンツキー
+ * @return array data
+ */
+	public function readyData($process, $pluginKey, $contentKey) {
+		// 登録
+		if ($process == $this::PROCESS_ADD) {
+			$data = array('ContentComment' => array(
+				'block_key' => $this->controller->viewVars['blockKey'],
+				'plugin_key' => $pluginKey,
+				'content_key' => $contentKey,
+				'status' => ContentComment::STATUS_PUBLISHED, // 公開
+				'comment' => $this->controller->data['contentComment']['comment'],
+			));
+
+			// 編集
+		} elseif ($process == $this::PROCESS_EDIT) {
+			$data = array('ContentComment' => array(
+				'id' => $this->controller->data['contentComment']['id'],
+				'block_key' => $this->controller->viewVars['blockKey'],
+				'plugin_key' => $pluginKey,
+				'content_key' => $contentKey,
+				'comment' => $this->controller->data['contentComment']['comment'],
+			));
+
+			// 承認
+		} elseif ($process == $this::PROCESS_APPROVED) {
+			$data = array('ContentComment' => array(
+				'id' => $this->controller->data['contentComment']['id'],
+				'block_key' => $this->controller->viewVars['blockKey'],
+				'plugin_key' => $pluginKey,
+				'content_key' => $contentKey,
+				'status' => ContentComment::STATUS_PUBLISHED, // 公開
+			));
+		}
+
+		return $data;
 	}
 }
